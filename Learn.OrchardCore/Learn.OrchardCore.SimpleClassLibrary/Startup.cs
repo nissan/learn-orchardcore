90032422678d8468c0dd6095af12050ee403ddd8
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-06-28 14:25:48 +1000

    Add simple class library

---
+++ Learn.OrchardCore/Learn.OrchardCore.SimpleClassLibrary/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Learn.OrchardCore.SimpleClassLibrary
+{
+    public class Startup
+    {
+        public void Configure(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.Map("/simpleHello", async context =>
+            {
+                await context.Response.WriteAsync("Hello there!");
+            });
+        }
+    }
+}
