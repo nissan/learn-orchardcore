dfe7f246b1cf17f42547048eb24f57234aafd399
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-06-28 15:15:27 +1000

    Add OrchardCore bits, link to module project

--- Learn.OrchardCore/Learn.OrchardCore.WebApplication/Startup.cs
+++ Learn.OrchardCore/Learn.OrchardCore.WebApplication/Startup.cs
@@ -23,7 +23,7 @@ namespace Learn.OrchardCore.WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddOrchardCore().AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,12 +45,7 @@ namespace Learn.OrchardCore.WebApplication
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseOrchardCore();
         }
     }
 }
